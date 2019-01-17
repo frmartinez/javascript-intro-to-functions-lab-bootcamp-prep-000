beff422d9512ef4cb66c5021ce46e8e1a7573386
Fernando Martinez <martinezfer@gmail.com>
2019-01-17 19:26:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-    if (string.toLowerCase() === 'hello') {
-        return "I can't hear you!";
-    }
-    else if (string.toUpperCase() === 'HELLO') {
-        return "YES INDEED!";
-    }
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+}
+else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+} else if ('I love you, Grandma.') {
+  return 'I love you, too.'
 }
 
 
